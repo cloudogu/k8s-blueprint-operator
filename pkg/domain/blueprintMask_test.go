14ef184d5396a808a39621494d17d48afb95ed93
Alexander Dammeier <alexander.dammeier@cloudogu.com>
2023-11-29 18:26:56 +0100

    #4 fix test names

--- pkg/domain/blueprintMask_test.go
+++ pkg/domain/blueprintMask_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func Test_blueprintMaskV1Validator_Validate(t *testing.T) {
+func Test_Validate(t *testing.T) {
 	dogus := []MaskTargetDogu{
 		{Namespace: "absent", Name: "dogu1", Version: "3.2.1-0", TargetState: TargetStateAbsent},
 		{Namespace: "absent", Name: "dogu2", TargetState: TargetStateAbsent},
@@ -20,7 +20,7 @@ func Test_blueprintMaskV1Validator_Validate(t *testing.T) {
 	require.Nil(t, err)
 }
 
-func Test_blueprintMaskV1Validator_ValidateWithMissingDoguName(t *testing.T) {
+func Test_ValidateWithMissingDoguName(t *testing.T) {
 	dogus := []MaskTargetDogu{
 		{TargetState: TargetStatePresent},
 	}
@@ -31,7 +31,7 @@ func Test_blueprintMaskV1Validator_ValidateWithMissingDoguName(t *testing.T) {
 	require.NotNil(t, err, "A missing dogu name definition in a dogu should lead to an error")
 }
 
-func Test_blueprintMaskV1Validator_ValidateWithDuplicatedDoguNames(t *testing.T) {
+func Test_ValidateWithDuplicatedDoguNames(t *testing.T) {
 	dogus := []MaskTargetDogu{
 		{Name: "present/dogu4", TargetState: TargetStatePresent},
 		{Name: "present/dogu4", TargetState: TargetStateAbsent},
