deb2628232d834be2d1a7aa190245ea58974fe2f
Alexander Dammeier <alexander.dammeier@cloudogu.com>
2023-11-29 12:01:58 +0100

    #4 replace repo.save functions with create and update

--- pkg/application/adpaterinterfaces.go
+++ pkg/application/adpaterinterfaces.go
@@ -8,13 +8,15 @@ import (
 type DoguInstallationRepository interface {
 	getByName(doguName string) (ecosystem.DoguInstallation, error)
 	getAll() ([]ecosystem.DoguInstallation, error)
-	save(ecosystem.DoguInstallation) error
+	create(ecosystem.DoguInstallation) error
+	update(ecosystem.DoguInstallation) error
 	delete(ecosystem.DoguInstallation) error
 }
 
 type BlueprintRepository interface {
 	getById(doguName string) (domain.BlueprintV2, error)
 	getAll() ([]domain.BlueprintV2, error)
-	save(domain.BlueprintV2) error
+	create(domain.BlueprintV2) error
+	update(domain.BlueprintV2) error
 	delete(domain.BlueprintV2) error
 }
