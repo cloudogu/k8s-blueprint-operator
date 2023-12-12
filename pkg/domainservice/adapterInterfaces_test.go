73605bb589219a99cf2f862bf35e3c45eeec8fae
Alexander Dammeier <alexander.dammeier@cloudogu.com>
2023-12-12 15:34:26 +0100

    #4 add tests for ConflictError

--- pkg/domainservice/adapterInterfaces_test.go
+++ pkg/domainservice/adapterInterfaces_test.go
@@ -51,3 +51,26 @@ func TestNotFoundError_Unwrap(t *testing.T) {
 		assert.ErrorContains(t, actual.Unwrap(), "test2")
 	})
 }
+
+func TestConflictError_Error(t *testing.T) {
+	t.Run("without wrapped error", func(t *testing.T) {
+		actual := ConflictError{WrappedError: nil, Message: "test"}
+		assert.Equal(t, "test", actual.Error())
+	})
+	t.Run("with wrapped error", func(t *testing.T) {
+		actual := ConflictError{WrappedError: errors.New("test2"), Message: "test"}
+		assert.Equal(t, "test: test2", actual.Error())
+	})
+}
+
+func TestConflictError_Unwrap(t *testing.T) {
+	t.Run("without wrapped error", func(t *testing.T) {
+		actual := ConflictError{WrappedError: nil, Message: "test"}
+		assert.Nil(t, actual.Unwrap())
+	})
+	t.Run("with wrapped error", func(t *testing.T) {
+		actual := ConflictError{WrappedError: errors.New("test2"), Message: "test"}
+		assert.Error(t, actual.Unwrap())
+		assert.ErrorContains(t, actual.Unwrap(), "test2")
+	})
+}
