cac229c7f5965452a51f8a4730e46844728f2e4c
Alexander Dammeier <alexander.dammeier@cloudogu.com>
2023-12-19 12:05:43 +0100

    #7 model doguInstallation aggregate

--- pkg/domain/ecosystem/doguInstallation.go
+++ pkg/domain/ecosystem/doguInstallation.go
@@ -1,14 +1,24 @@
 package ecosystem
 
 type DoguInstallation struct {
-	Name    string
-	Version string
-	Health  DoguHealth
+	Name          string
+	Version       string
+	Health        HealthStatus
+	UpgradeConfig UpgradeConfig
 }
 
-type DoguHealth = string
+type HealthStatus = string
 
 const (
-	Healhty   DoguHealth = "healthy"
-	Unhealthy DoguHealth = "unhealthy"
+	PendingHealthStatus     HealthStatus = ""
+	AvailableHealthStatus   HealthStatus = "available"
+	UnavailableHealthStatus HealthStatus = "unavailable"
 )
+
+// UpgradeConfig contains configuration hints regarding aspects during the upgrade of dogus.
+type UpgradeConfig struct {
+	// AllowNamespaceSwitch lets a dogu switch its dogu namespace during an upgrade. The dogu must be technically the
+	// same dogu which did reside in a different namespace. The remote dogu's version must be equal to or greater than
+	// the version of the local dogu.
+	AllowNamespaceSwitch bool `json:"allowNamespaceSwitch,omitempty"`
+}
