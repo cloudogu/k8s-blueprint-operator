d5511255e591a377cd725bc27a0f8d4dc7f1ccfa
Alexander Dammeier <alexander.dammeier@cloudogu.com>
2023-12-19 13:44:20 +0100

    #7 add namespace, version, status and persistenceContext to the doguInstallation aggregate

--- pkg/domain/ecosystem/doguInstallation.go
+++ pkg/domain/ecosystem/doguInstallation.go
@@ -1,11 +1,36 @@
 package ecosystem
 
+import "github.com/cloudogu/cesapp-lib/core"
+
+// DoguInstallation represents an installed or to be installed dogu in the ecosystem.
 type DoguInstallation struct {
-	Name          string
-	Version       string
-	Health        HealthStatus
+	// Namespace is the namespace of the dogu, e.g. 'official' like in 'official/postgresql'
+	Namespace string
+	// Name is the simple name of the dogu, e.g. 'postgresql' like in 'official/postgresql'.
+	// the name is also the id of the dogu in the ecosystem as only one dogu with this name can be installed.
+	Name string
+	// Version is the version of the dogu
+	Version core.Version
+	// Status is the installation status of the dogu in the ecosystem
+	Status string
+	// Health is the current health status of the dogu in the ecosystem
+	Health HealthStatus
+	// UpgradeConfig contains configuration for dogu upgrades
 	UpgradeConfig UpgradeConfig
+	// PersistenceContext can hold generic values needed for persistence with repositories, e.g. version counters or transaction contexts.
+	// This field has a generic map type as the values within it highly depend on the used type of repository.
+	// This field should be ignored in the whole domain.
+	PersistenceContext map[string]interface{}
 }
+
+const (
+	DoguStatusNotInstalled = ""
+	DoguStatusInstalling   = "installing"
+	DoguStatusUpgrading    = "upgrading"
+	DoguStatusDeleting     = "deleting"
+	DoguStatusInstalled    = "installed"
+	DoguStatusPVCResizing  = "resizing PVC"
+)
 
 type HealthStatus = string
 
