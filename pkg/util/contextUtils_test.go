42daac13160e666f2f2e6ce78418402b6c9dd894
Alexander Dammeier <alexander.dammeier@cloudogu.com>
2024-01-15 14:29:32 +0100

    #11 add retry function which encapsulates cancellation handling

---
+++ pkg/util/contextUtils_test.go
@@ -0,0 +1,96 @@
+package util
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+type testType struct {
+	name string
+}
+
+func TestRetryUntilSuccessOrCancellation(t *testing.T) {
+	expected := &testType{"test"}
+
+	t.Run("ok", func(t *testing.T) {
+		testCtx, cancel := context.WithTimeout(context.Background(), 1*time.Millisecond)
+		defer cancel()
+
+		result, err := RetryUntilSuccessOrCancellation(
+			testCtx,
+			1*time.Second,
+			func(ctx context.Context) (*testType, error, bool) {
+				return expected, nil, false
+			},
+		)
+
+		require.NoError(t, err)
+		assert.Equal(t, expected, result)
+	})
+
+	t.Run("ok after retry", func(t *testing.T) {
+		testCtx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		defer cancel()
+
+		callCounter := 0
+
+		result, err := RetryUntilSuccessOrCancellation(
+			testCtx,
+			1*time.Second,
+			func(ctx context.Context) (*testType, error, bool) {
+				callCounter++
+				if callCounter >= 2 { // return at second try
+					return expected, nil, false
+				} else {
+					return nil, nil, true
+				}
+			},
+		)
+
+		require.NoError(t, err)
+		assert.Equal(t, expected, result)
+	})
+
+	t.Run("timeout in given function", func(t *testing.T) {
+		testCtx, cancel := context.WithTimeout(context.Background(), 0*time.Millisecond)
+		defer cancel()
+
+		result, err := RetryUntilSuccessOrCancellation(
+			testCtx,
+			1*time.Second,
+			func(ctx context.Context) (*testType, error, bool) {
+				select {
+				case <-ctx.Done():
+					return expected, ctx.Err(), false
+				default:
+					return nil, nil, true
+				}
+			},
+		)
+
+		require.ErrorIs(t, err, context.DeadlineExceeded)
+		assert.Equal(t, expected, result)
+	})
+
+	t.Run("timeout outside of given function", func(t *testing.T) {
+		testCtx, cancel := context.WithTimeout(context.Background(), 0*time.Millisecond)
+		defer cancel()
+
+		result, err := RetryUntilSuccessOrCancellation(
+			testCtx,
+			1*time.Second,
+			func(ctx context.Context) (*testType, error, bool) {
+				//ignore timeout here, so that the helper function need to stop
+				return nil, nil, true
+			},
+		)
+
+		require.ErrorIs(t, err, context.DeadlineExceeded)
+		var expectedNil *testType = nil
+		assert.Equal(t, expectedNil, result)
+	})
+
+}
