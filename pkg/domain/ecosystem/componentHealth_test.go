26bdf67df33d4daf34e68574b2c6dbcd1fcb6ec0
Jeremias Weber <jeremias.weber@cloudogu.com>
2024-01-25 09:22:19 +0100

    #15 test component + dogu health

--- pkg/domain/ecosystem/componentHealth_test.go
+++ pkg/domain/ecosystem/componentHealth_test.go
@@ -20,7 +20,7 @@ func TestComponentHealthResult_String(t *testing.T) {
 		{
 			name: "only available components should result in 0 components unhealthy",
 			healthStates: map[HealthStatus][]ComponentName{
-				AvailableHealthStatus: {ComponentName("k8s-dogu-operator")},
+				AvailableHealthStatus: {"k8s-dogu-operator"},
 			},
 			contains:    []string{"0 components are unhealthy: "},
 			notContains: []string{"k8s-dogu-operator"},
@@ -28,10 +28,10 @@ func TestComponentHealthResult_String(t *testing.T) {
 		{
 			name: "any components not available should be unhealthy",
 			healthStates: map[HealthStatus][]ComponentName{
-				AvailableHealthStatus:    {ComponentName("k8s-blueprint-operator")},
-				UnavailableHealthStatus:  {ComponentName("k8s-etcd"), ComponentName("k8s-dogu-operator")},
-				NotInstalledHealthStatus: {ComponentName("k8s-service-discovery")},
-				"other":                  {ComponentName("k8s-component-operator")},
+				AvailableHealthStatus:    {"k8s-blueprint-operator"},
+				UnavailableHealthStatus:  {"k8s-etcd", "k8s-dogu-operator"},
+				NotInstalledHealthStatus: {"k8s-service-discovery"},
+				"other":                  {"k8s-component-operator"},
 			},
 			contains: []string{
 				"4 components are unhealthy: ",
@@ -53,6 +53,127 @@ func TestComponentHealthResult_String(t *testing.T) {
 			for _, notContains := range tt.notContains {
 				assert.NotContains(t, actual, notContains)
 			}
+		})
+	}
+}
+
+func TestComponentHealthResult_AllHealthy(t *testing.T) {
+	tests := []struct {
+		name         string
+		healthStates map[HealthStatus][]ComponentName
+		want         bool
+	}{
+		{
+			name:         "should be healthy if empty",
+			healthStates: map[HealthStatus][]ComponentName{},
+			want:         true,
+		},
+		{
+			name: "should be healthy if all are available",
+			healthStates: map[HealthStatus][]ComponentName{
+				AvailableHealthStatus: {"k8s-blueprint-operator", "k8s-etcd", "k8s-service-discovery"},
+			},
+			want: true,
+		},
+		{
+			name: "should not be healthy if one is not available",
+			healthStates: map[HealthStatus][]ComponentName{
+				AvailableHealthStatus:   {"k8s-blueprint-operator", "k8s-etcd", "k8s-service-discovery"},
+				UnavailableHealthStatus: {"k8s-dogu-operator"},
+			},
+			want: false,
+		},
+		{
+			name: "should not be healthy if multiple are not available",
+			healthStates: map[HealthStatus][]ComponentName{
+				AvailableHealthStatus:    {"k8s-blueprint-operator", "k8s-etcd", "k8s-service-discovery"},
+				UnavailableHealthStatus:  {"k8s-dogu-operator", "k8s-component-operator"},
+				PendingHealthStatus:      {"k8s-longhorn"},
+				NotInstalledHealthStatus: {"k8s-backup-operator"},
+				"other":                  {"k8s-velero"},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := ComponentHealthResult{ComponentsByStatus: tt.healthStates}
+			assert.Equalf(t, tt.want, result.AllHealthy(), "AllHealthy()")
+		})
+	}
+}
+
+func TestCalculateComponentHealthResult(t *testing.T) {
+	type args struct {
+		installedComponents map[string]*ComponentInstallation
+		requiredComponents  []RequiredComponent
+	}
+	tests := []struct {
+		name string
+		args args
+		want ComponentHealthResult
+	}{
+		{
+			name: "result should be empty for no required and no installed components",
+			args: args{
+				installedComponents: map[string]*ComponentInstallation{},
+				requiredComponents:  []RequiredComponent{},
+			},
+			want: ComponentHealthResult{ComponentsByStatus: map[HealthStatus][]ComponentName{}},
+		},
+		{
+			name: "result should contain components that are not installed but required",
+			args: args{
+				installedComponents: map[string]*ComponentInstallation{},
+				requiredComponents:  []RequiredComponent{{Name: "k8s-etcd"}, {Name: "k8s-service-discovery"}},
+			},
+			want: ComponentHealthResult{ComponentsByStatus: map[HealthStatus][]ComponentName{
+				NotInstalledHealthStatus: {"k8s-etcd", "k8s-service-discovery"},
+			}},
+		},
+		{
+			name: "result should contain any components with their health state",
+			args: args{
+				installedComponents: map[string]*ComponentInstallation{
+					"k8s-blueprint-operator": {Name: "k8s-blueprint-operator", Health: AvailableHealthStatus},
+					"k8s-dogu-operator":      {Name: "k8s-dogu-operator", Health: UnavailableHealthStatus},
+					"k8s-component-operator": {Name: "k8s-component-operator", Health: UnavailableHealthStatus},
+					"k8s-longhorn":           {Name: "k8s-longhorn", Health: PendingHealthStatus},
+					"k8s-velero":             {Name: "k8s-velero", Health: "other"},
+				},
+				requiredComponents: []RequiredComponent{},
+			},
+			want: ComponentHealthResult{ComponentsByStatus: map[HealthStatus][]ComponentName{
+				AvailableHealthStatus:   {"k8s-blueprint-operator"},
+				UnavailableHealthStatus: {"k8s-dogu-operator", "k8s-component-operator"},
+				PendingHealthStatus:     {"k8s-longhorn"},
+				"other":                 {"k8s-velero"},
+			}},
+		},
+		{
+			name: "result should contain any components with their health state and components that are not installed but required",
+			args: args{
+				installedComponents: map[string]*ComponentInstallation{
+					"k8s-blueprint-operator": {Name: "k8s-blueprint-operator", Health: AvailableHealthStatus},
+					"k8s-dogu-operator":      {Name: "k8s-dogu-operator", Health: UnavailableHealthStatus},
+					"k8s-component-operator": {Name: "k8s-component-operator", Health: UnavailableHealthStatus},
+					"k8s-longhorn":           {Name: "k8s-longhorn", Health: PendingHealthStatus},
+					"k8s-velero":             {Name: "k8s-velero", Health: "other"},
+				},
+				requiredComponents: []RequiredComponent{{Name: "k8s-etcd"}, {Name: "k8s-service-discovery"}},
+			},
+			want: ComponentHealthResult{ComponentsByStatus: map[HealthStatus][]ComponentName{
+				AvailableHealthStatus:    {"k8s-blueprint-operator"},
+				UnavailableHealthStatus:  {"k8s-dogu-operator", "k8s-component-operator"},
+				PendingHealthStatus:      {"k8s-longhorn"},
+				"other":                  {"k8s-velero"},
+				NotInstalledHealthStatus: {"k8s-etcd", "k8s-service-discovery"},
+			}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, CalculateComponentHealthResult(tt.args.installedComponents, tt.args.requiredComponents), "CalculateComponentHealthResult(%v, %v)", tt.args.installedComponents, tt.args.requiredComponents)
 		})
 	}
 }
--- pkg/domain/ecosystem/doguHealth_test.go
+++ pkg/domain/ecosystem/doguHealth_test.go
@@ -58,7 +58,7 @@ func TestDoguHealthResult_String(t *testing.T) {
 		{
 			name: "only available dogus should result in 0 components unhealthy",
 			healthStates: map[HealthStatus][]DoguName{
-				AvailableHealthStatus: {DoguName("nginx-ingress")},
+				AvailableHealthStatus: {"nginx-ingress"},
 			},
 			contains:    []string{"0 dogus are unhealthy: "},
 			notContains: []string{"nginx-ingress"},
@@ -66,9 +66,9 @@ func TestDoguHealthResult_String(t *testing.T) {
 		{
 			name: "any dogus not available should be unhealthy",
 			healthStates: map[HealthStatus][]DoguName{
-				AvailableHealthStatus:   {DoguName("nginx-static")},
-				UnavailableHealthStatus: {DoguName("postgresql"), DoguName("redmine")},
-				"other":                 {DoguName("scm")},
+				AvailableHealthStatus:   {"nginx-static"},
+				UnavailableHealthStatus: {"postgresql", "redmine"},
+				"other":                 {"scm"},
 			},
 			contains: []string{
 				"3 dogus are unhealthy: ",
@@ -89,6 +89,51 @@ func TestDoguHealthResult_String(t *testing.T) {
 			for _, notContains := range tt.notContains {
 				assert.NotContains(t, actual, notContains)
 			}
+		})
+	}
+}
+
+func TestDoguHealthResult_AllHealthy(t *testing.T) {
+	tests := []struct {
+		name         string
+		healthStates map[HealthStatus][]DoguName
+		want         bool
+	}{
+		{
+			name:         "should be healthy if empty",
+			healthStates: map[HealthStatus][]DoguName{},
+			want:         true,
+		},
+		{
+			name: "should be healthy if all are available",
+			healthStates: map[HealthStatus][]DoguName{
+				AvailableHealthStatus: {"nginx-ingress", "nginx-static", "postfix"},
+			},
+			want: true,
+		},
+		{
+			name: "should not be healthy if one is not available",
+			healthStates: map[HealthStatus][]DoguName{
+				AvailableHealthStatus:   {"nginx-ingress", "nginx-static", "postfix"},
+				UnavailableHealthStatus: {"ldap"},
+			},
+			want: false,
+		},
+		{
+			name: "should not be healthy if multiple are not available",
+			healthStates: map[HealthStatus][]DoguName{
+				AvailableHealthStatus:   {"nginx-ingress", "nginx-static", "postfix"},
+				UnavailableHealthStatus: {"ldap", "redmine"},
+				PendingHealthStatus:     {"postgresql"},
+				"other":                 {"plantuml"},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result := DoguHealthResult{DogusByStatus: tt.healthStates}
+			assert.Equalf(t, tt.want, result.AllHealthy(), "AllHealthy()")
 		})
 	}
 }
