c9d2fe1b9e37297aa921e7352fd22bb56c05ba32
Jeremias Weber <jeremias.weber@cloudogu.com>
2024-02-08 14:09:44 +0100

    #15 Move health config to serializer and comment

--- pkg/adapter/kubernetes/config/healthSerializer.go
+++ pkg/adapter/kubernetes/config/healthSerializer.go
@@ -6,6 +6,30 @@ import (
 	"time"
 )
 
+type healthConfig struct {
+	// Components contains configuration concerning the health-check of components.
+	Components componentHealthConfig `yaml:"components,omitempty" json:"components,omitempty"`
+	// Wait contains configuration concerning how the stand-by-period for the ecosystem to become healthy.
+	Wait waitHealthConfig `yaml:"wait,omitempty" json:"wait,omitempty"`
+}
+
+type componentHealthConfig struct {
+	// Required is a list of components that have to be installed for the health-check to succeed.
+	Required []requiredComponent `yaml:"required,omitempty" json:"required,omitempty"`
+}
+
+type requiredComponent struct {
+	// Name identifies the component.
+	Name string `yaml:"name" json:"name"`
+}
+
+type waitHealthConfig struct {
+	// Timeout is the maximum time to wait for the ecosystem to become healthy.
+	Timeout duration `yaml:"timeout,omitempty" json:"timeout,omitempty"`
+	// Interval is the time to wait between health checks.
+	Interval duration `yaml:"interval,omitempty" json:"interval,omitempty"`
+}
+
 // duration is a wrapper for time.Duration so that it can be marshalled and unmarshalled to JSON
 // using time.Duration's string formatting.
 type duration struct {
