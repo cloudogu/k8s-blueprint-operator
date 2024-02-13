3e70af3feefec8361d25195298670665d35361a5
Alexander Dammeier <alexander.dammeier@cloudogu.com>
2024-02-13 15:32:36 +0100

    #34 add ConfigDiff model
    Co-authored-by: Jeremias Weber <jeremias.weber@cloudogu.com>
    Co-authored-by: Niklas <niklas.hinze@cloudogu.com>

--- pkg/domain/stateDiff.go
+++ pkg/domain/stateDiff.go
@@ -3,8 +3,52 @@ package domain
 // StateDiff represents the diff between the defined state in the effective blueprint and the actual state in the ecosystem.
 // If there is a state in the ecosystem, which is not represented in the effective blueprint, then the expected state is the actual state.
 type StateDiff struct {
-	DoguDiffs      DoguDiffs
-	ComponentDiffs ComponentDiffs
+	DoguDiffs        DoguDiffs
+	ComponentDiffs   ComponentDiffs
+	DoguConfigDiff   map[string]DoguConfigDiff
+	GlobalConfigDiff GlobalConfigDiff
+}
+
+type DoguConfigDiff struct {
+	SensibleDoguConfigDiff SensibleDoguConfigDiff
+	NormalDoguConfigDiff   NormalDoguConfigDiff
+}
+
+type SensibleDoguConfigDiff []ConfigKeyDiff
+
+type NormalDoguConfigDiff []ConfigKeyDiff
+
+type ConfigKeyDiff struct {
+	Key      string
+	Actual   ConfigValue
+	Expected ConfigValue
+	Action   ConfigAction
+}
+
+type ConfigValue struct {
+	Value  string
+	Exists bool
+}
+
+type GlobalConfigDiff []ConfigKeyDiff
+
+type ConfigAction string
+
+const (
+	ConfigActionNone   ConfigAction = "none"
+	ConfigActionSet    ConfigAction = "set"
+	ConfigActionRemove ConfigAction = "remove"
+)
+
+func getNeededConfigAction(expected ConfigValue, actual ConfigValue) ConfigAction {
+	if expected == actual {
+		return ConfigActionNone
+	}
+	if !expected.Exists {
+		return ConfigActionRemove
+	}
+
+	return ConfigActionSet
 }
 
 // Action represents a needed Action for a dogu to reach the expected state.
