b227c807e87d9b309223d4dbaeb5a6d600d33fe1
Alexander Dammeier <alexander.dammeier@cloudogu.com>
2024-02-19 16:59:52 +0100

    #41 determine doguConfigDiff

--- pkg/domain/stateDiffConfig.go
+++ pkg/domain/stateDiffConfig.go
@@ -1,6 +1,9 @@
 package domain
 
-import "github.com/cloudogu/k8s-blueprint-operator/pkg/domain/common"
+import (
+	"github.com/cloudogu/k8s-blueprint-operator/pkg/domain/common"
+	"github.com/cloudogu/k8s-blueprint-operator/pkg/domain/ecosystem"
+)
 
 type CombinedDoguConfigDiff struct {
 	DoguConfigDiff          DoguConfigDiff
@@ -48,6 +51,72 @@ const (
 	ConfigActionSet    ConfigAction = "set"
 	ConfigActionRemove ConfigAction = "remove"
 )
+
+func determineConfigDiff(
+	blueprintConfig Config,
+	actualGlobalConfig map[common.GlobalConfigKey]ecosystem.GlobalConfigEntry,
+	actualDoguConfig map[common.DoguConfigKey]ecosystem.DoguConfigEntry,
+	actualSensitiveDoguConfig map[common.SensitiveDoguConfigKey]ecosystem.SensitiveDoguConfigEntry,
+) (CombinedDoguConfigDiff, GlobalConfigDiff, error) {
+	return CombinedDoguConfigDiff{
+			DoguConfigDiff:          determineDoguConfigDiffs(blueprintConfig.Dogus, actualDoguConfig),
+			SensitiveDoguConfigDiff: determineSensitiveDoguConfigDiff(blueprintConfig.Dogus, actualSensitiveDoguConfig),
+		},
+		determineGlobalConfigDiff(blueprintConfig.Global, actualGlobalConfig),
+		nil
+}
+
+func determineDoguConfigDiffs(
+	config map[common.SimpleDoguName]CombinedDoguConfig,
+	actualDoguConfig map[common.DoguConfigKey]ecosystem.DoguConfigEntry,
+) DoguConfigDiff {
+	var doguConfigDiff []DoguConfigEntryDiff
+
+	for _, doguConfig := range config {
+		// present entries
+		for key, expectedValue := range doguConfig.Config.Present {
+			actualEntry, actualExists := actualDoguConfig[key]
+			doguConfigDiff = append(doguConfigDiff, determineDoguConfigDiff(key, string(actualEntry.Value), actualExists, string(expectedValue), true))
+		}
+		// absent entries
+		for _, key := range doguConfig.Config.Absent {
+			actualEntry, actualExists := actualDoguConfig[key]
+			doguConfigDiff = append(doguConfigDiff, determineDoguConfigDiff(key, string(actualEntry.Value), actualExists, string(actualEntry.Value), false))
+		}
+	}
+	return doguConfigDiff
+}
+
+func determineDoguConfigDiff(key common.DoguConfigKey, actualValue string, actualExists bool, expectedValue string, expectedExists bool) DoguConfigEntryDiff {
+	actual := DoguConfigValueState{
+		Value:  actualValue,
+		Exists: actualExists,
+	}
+	expected := DoguConfigValueState{
+		Value:  expectedValue,
+		Exists: expectedExists,
+	}
+	return DoguConfigEntryDiff{
+		Key:      key,
+		Actual:   actual,
+		Expected: expected,
+		Action:   getNeededConfigAction(ConfigValueState(actual), ConfigValueState(expected)),
+	}
+}
+
+func determineSensitiveDoguConfigDiff(
+	config map[common.SimpleDoguName]CombinedDoguConfig,
+	actualDoguConfig map[common.SensitiveDoguConfigKey]ecosystem.SensitiveDoguConfigEntry,
+) SensitiveDoguConfigDiff {
+	return nil
+}
+
+func determineGlobalConfigDiff(
+	config GlobalConfig,
+	actualDoguConfig map[common.GlobalConfigKey]ecosystem.GlobalConfigEntry,
+) GlobalConfigDiff {
+	return nil
+}
 
 func getNeededConfigAction(expected ConfigValueState, actual ConfigValueState) ConfigAction {
 	if expected == actual {
