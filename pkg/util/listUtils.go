7a233270e161908f60a5f4965805438a72d1364e
Jeremias Weber <jeremias.weber@cloudogu.com>
2024-02-19 10:35:12 +0100

    #37 make GetDuplicates generic

--- pkg/util/listUtils.go
+++ pkg/util/listUtils.go
@@ -1,10 +1,10 @@
 package util
 
-func GetDuplicates(list []string) []string {
+func GetDuplicates[T comparable](list []T) []T {
 	elementCount := CountGrouped(list)
 
 	// get list of names with count != 1
-	var duplicates []string
+	var duplicates []T
 	for name, count := range elementCount {
 		if count != 1 {
 			duplicates = append(duplicates, name)
