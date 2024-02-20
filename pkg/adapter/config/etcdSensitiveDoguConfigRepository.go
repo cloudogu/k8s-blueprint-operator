c09e684a663d4dd2fd123d535e25beacdacf581a
Jeremias Weber <jeremias.weber@cloudogu.com>
2024-02-20 08:52:12 +0100

    #42 GetAll and SaveAll for sensitive dogu config
    also improve error handling

--- pkg/adapter/config/etcdSensitiveDoguConfigRepository.go
+++ pkg/adapter/config/etcdSensitiveDoguConfigRepository.go
@@ -2,10 +2,11 @@ package config
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"github.com/cloudogu/cesapp-lib/registry"
 	"github.com/cloudogu/k8s-blueprint-operator/pkg/domain/common"
 	"github.com/cloudogu/k8s-blueprint-operator/pkg/domain/ecosystem"
+	"github.com/cloudogu/k8s-blueprint-operator/pkg/domainservice"
 )
 
 type EtcdSensitiveDoguConfigRepository struct {
@@ -16,9 +17,26 @@ func NewEtcdSensitiveDoguConfigRepository(etcdStore etcdStore) *EtcdSensitiveDog
 	return &EtcdSensitiveDoguConfigRepository{etcdStore: etcdStore}
 }
 
-func (e EtcdSensitiveDoguConfigRepository) GetAllByKey(ctx context.Context, keys []common.SensitiveDoguConfigKey) (map[common.SimpleDoguName][]*ecosystem.SensitiveDoguConfigEntry, error) {
-	// TODO implement me
-	panic("implement me")
+func (e EtcdSensitiveDoguConfigRepository) GetAllByKey(_ context.Context, keys []common.SensitiveDoguConfigKey) (map[common.SimpleDoguName][]*ecosystem.SensitiveDoguConfigEntry, error) {
+	var errs []error
+	var entriesByDogu map[common.SimpleDoguName][]*ecosystem.SensitiveDoguConfigEntry
+	for _, key := range keys {
+		entryRaw, err := e.etcdStore.DoguConfig(string(key.DoguName)).Get(key.Key)
+		if registry.IsKeyNotFoundError(err) {
+			errs = append(errs, domainservice.NewNotFoundError(err, "could not find %q in etcd", key))
+			continue
+		} else if err != nil {
+			errs = append(errs, domainservice.NewInternalError(err, "failed to get %q from etcd", key))
+			continue
+		}
+
+		entriesByDogu[key.DoguName] = append(entriesByDogu[key.DoguName], &ecosystem.SensitiveDoguConfigEntry{
+			Key:   common.SensitiveDoguConfigKey{DoguConfigKey: common.DoguConfigKey{DoguName: key.DoguName, Key: key.Key}},
+			Value: common.EncryptedDoguConfigValue(entryRaw),
+		})
+	}
+
+	return entriesByDogu, errors.Join(errs...)
 }
 
 func (e EtcdSensitiveDoguConfigRepository) Save(_ context.Context, entry *ecosystem.SensitiveDoguConfigEntry) error {
@@ -27,15 +45,25 @@ func (e EtcdSensitiveDoguConfigRepository) Save(_ context.Context, entry *ecosys
 	strValue := string(entry.Value)
 	err := setEtcdKey(strKey, strValue, e.etcdStore.DoguConfig(strDoguName))
 	if err != nil {
-		return fmt.Errorf("failed to set encrypted config key %q with value %q for dogu %q: %w", strKey, strValue, strDoguName, err)
+		return domainservice.NewInternalError(err, "failed to set encrypted config key %q with value %q for dogu %q", strKey, strValue, strDoguName)
 	}
 
 	return nil
 }
 
-func (e EtcdSensitiveDoguConfigRepository) SaveAll(ctx context.Context, keys []*ecosystem.SensitiveDoguConfigEntry) error {
-	// TODO implement me
-	panic("implement me")
+func (e EtcdSensitiveDoguConfigRepository) SaveAll(ctx context.Context, entries []*ecosystem.SensitiveDoguConfigEntry) error {
+	var errs []error
+	for _, entry := range entries {
+		err := e.Save(ctx, entry)
+		errs = append(errs, err)
+	}
+
+	err := errors.Join(errs...)
+	if err != nil {
+		return domainservice.NewInternalError(err, "failed to set given dogu config entries in etcd")
+	}
+
+	return nil
 }
 
 func (e EtcdSensitiveDoguConfigRepository) Delete(_ context.Context, key common.SensitiveDoguConfigKey) error {
@@ -43,7 +71,7 @@ func (e EtcdSensitiveDoguConfigRepository) Delete(_ context.Context, key common.
 	strKey := key.Key
 	err := deleteEtcdKey(strKey, e.etcdStore.DoguConfig(strDoguName))
 	if err != nil && !registry.IsKeyNotFoundError(err) {
-		return fmt.Errorf("failed to delete encrypted config key %q for dogu %q: %w", strKey, strDoguName, err)
+		return domainservice.NewInternalError(err, "failed to delete encrypted config key %q for dogu %q", strKey, strDoguName)
 	}
 
 	return nil
