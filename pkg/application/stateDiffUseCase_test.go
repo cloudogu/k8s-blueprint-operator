07fa89cac9a7a1130390f7fb88c15ab49dde98d3
Alexander Dammeier <alexander.dammeier@cloudogu.com>
2024-02-22 10:54:54 +0100

    #41 make DoguAlreadyInstalled in diff public

--- pkg/application/stateDiffUseCase_test.go
+++ pkg/application/stateDiffUseCase_test.go
@@ -30,6 +30,13 @@ var nginxStaticQualifiedDoguName = common.QualifiedDoguName{
 	SimpleName: "nginx-static",
 }
 
+var (
+	nginxStaticConfigKeyNginxKey1          = common.DoguConfigKey{DoguName: "nginx-static", Key: "nginxKey1"}
+	nginxStaticConfigKeyNginxKey2          = common.DoguConfigKey{DoguName: "nginx-static", Key: "nginxKey2"}
+	nginxStaticSensitiveConfigKeyNginxKey1 = common.SensitiveDoguConfigKey{DoguConfigKey: nginxStaticConfigKeyNginxKey1}
+	nginxStaticSensitiveConfigKeyNginxKey2 = common.SensitiveDoguConfigKey{DoguConfigKey: nginxStaticConfigKeyNginxKey2}
+)
+
 func TestStateDiffUseCase_DetermineStateDiff(t *testing.T) {
 	t.Run("should fail to load blueprint spec", func(t *testing.T) {
 		// given
@@ -250,7 +257,7 @@ func TestStateDiffUseCase_DetermineStateDiff(t *testing.T) {
 		assert.ErrorIs(t, err, assert.AnError)
 		assert.ErrorContains(t, err, "cannot save blueprint spec \"testBlueprint1\" after determining the state diff to the ecosystem")
 	})
-	t.Run("should succeed", func(t *testing.T) {
+	t.Run("should succeed for dogu diff", func(t *testing.T) {
 		// given
 		blueprint := &domain.BlueprintSpec{
 			Id: "testBlueprint1",
@@ -278,7 +285,6 @@ func TestStateDiffUseCase_DetermineStateDiff(t *testing.T) {
 				},
 			},
 			Status: domain.StatusPhaseValidated,
-			//TODO: add config to test
 		}
 
 		blueprintRepoMock := newMockBlueprintSpecRepository(t)
@@ -295,11 +301,12 @@ func TestStateDiffUseCase_DetermineStateDiff(t *testing.T) {
 		componentInstallRepoMock := newMockComponentInstallationRepository(t)
 		componentInstallRepoMock.EXPECT().GetAll(testCtx).Return(nil, nil)
 		globalConfigRepoMock := NewMockGlobalConfigEntryRepository(t)
-		globalConfigRepoMock.EXPECT().GetAllByKey(testCtx, blueprint.EffectiveBlueprint.Config.Global.GetGlobalConfigKeys()).Return(map[common.GlobalConfigKey]*ecosystem.GlobalConfigEntry{}, nil)
+		globalConfigRepoMock.EXPECT().GetAllByKey(testCtx, []common.GlobalConfigKey(nil)).Return(map[common.GlobalConfigKey]*ecosystem.GlobalConfigEntry{}, nil)
 		doguConfigRepoMock := NewMockDoguConfigEntryRepository(t)
 		doguConfigRepoMock.EXPECT().GetAllByKey(testCtx, []common.DoguConfigKey(nil)).Return(map[common.DoguConfigKey]*ecosystem.DoguConfigEntry{}, nil)
 		sensitiveDoguConfigRepoMock := NewMockSensitiveDoguConfigEntryRepository(t)
 		sensitiveDoguConfigRepoMock.EXPECT().GetAllByKey(testCtx, []common.SensitiveDoguConfigKey(nil)).Return(map[common.SensitiveDoguConfigKey]*ecosystem.SensitiveDoguConfigEntry{}, nil)
+
 		sut := NewStateDiffUseCase(blueprintRepoMock, doguInstallRepoMock, componentInstallRepoMock, globalConfigRepoMock, doguConfigRepoMock, sensitiveDoguConfigRepoMock)
 
 		// when
@@ -361,6 +368,243 @@ func TestStateDiffUseCase_DetermineStateDiff(t *testing.T) {
 			},
 		}
 		assert.ElementsMatch(t, expectedDoguDiffs, blueprint.StateDiff.DoguDiffs)
+	})
+	t.Run("should succeed for global config diff", func(t *testing.T) {
+		// given
+		blueprint := &domain.BlueprintSpec{
+			Id: "testBlueprint1",
+			EffectiveBlueprint: domain.EffectiveBlueprint{
+				Config: domain.Config{
+					Global: domain.GlobalConfig{
+						Present: map[common.GlobalConfigKey]common.GlobalConfigValue{
+							"globalKey1": "globalValue",
+						},
+						Absent: []common.GlobalConfigKey{
+							"globalKey2",
+						},
+					},
+				},
+			},
+			Status: domain.StatusPhaseValidated,
+		}
+
+		blueprintRepoMock := newMockBlueprintSpecRepository(t)
+		blueprintRepoMock.EXPECT().GetById(testCtx, "testBlueprint1").Return(blueprint, nil)
+		blueprintRepoMock.EXPECT().Update(testCtx, blueprint).Return(nil)
+
+		doguInstallRepoMock := newMockDoguInstallationRepository(t)
+		installedDogus := map[common.SimpleDoguName]*ecosystem.DoguInstallation{
+			"nginx-static": {Name: nginxStaticQualifiedDoguName, Version: mustParseVersion(t, "1.8.6")},
+		}
+		doguInstallRepoMock.EXPECT().GetAll(testCtx).Return(installedDogus, nil)
+		componentInstallRepoMock := newMockComponentInstallationRepository(t)
+		componentInstallRepoMock.EXPECT().GetAll(testCtx).Return(nil, nil)
+		globalConfigRepoMock := NewMockGlobalConfigEntryRepository(t)
+		globalConfigRepoMock.EXPECT().
+			GetAllByKey(testCtx, blueprint.EffectiveBlueprint.Config.Global.GetGlobalConfigKeys()).
+			Return(map[common.GlobalConfigKey]*ecosystem.GlobalConfigEntry{}, nil)
+		doguConfigRepoMock := NewMockDoguConfigEntryRepository(t)
+		doguConfigRepoMock.EXPECT().GetAllByKey(testCtx, []common.DoguConfigKey(nil)).Return(map[common.DoguConfigKey]*ecosystem.DoguConfigEntry{}, nil)
+		sensitiveDoguConfigRepoMock := NewMockSensitiveDoguConfigEntryRepository(t)
+		sensitiveDoguConfigRepoMock.EXPECT().GetAllByKey(testCtx, []common.SensitiveDoguConfigKey(nil)).Return(map[common.SensitiveDoguConfigKey]*ecosystem.SensitiveDoguConfigEntry{}, nil)
+
+		sut := NewStateDiffUseCase(blueprintRepoMock, doguInstallRepoMock, componentInstallRepoMock, globalConfigRepoMock, doguConfigRepoMock, sensitiveDoguConfigRepoMock)
+
+		// when
+		err := sut.DetermineStateDiff(testCtx, "testBlueprint1")
+
+		// then
+		require.NoError(t, err)
+
+		expectedConfigDiff := []domain.GlobalConfigEntryDiff{
+			{
+				Key:          "globalKey1",
+				Actual:       domain.GlobalConfigValueState{Value: "", Exists: false},
+				Expected:     domain.GlobalConfigValueState{Value: "globalValue", Exists: true},
+				NeededAction: domain.ConfigActionSet,
+			},
+			{
+				Key:          "globalKey2",
+				Actual:       domain.GlobalConfigValueState{Value: "", Exists: false},
+				Expected:     domain.GlobalConfigValueState{Value: "", Exists: false},
+				NeededAction: domain.ConfigActionNone,
+			},
+		}
+		assert.ElementsMatch(t, expectedConfigDiff, blueprint.StateDiff.GlobalConfigDiffs)
+	})
+	t.Run("should succeed for dogu config diff", func(t *testing.T) {
+		// given
+		blueprint := &domain.BlueprintSpec{
+			Id: "testBlueprint1",
+			EffectiveBlueprint: domain.EffectiveBlueprint{
+				Config: domain.Config{
+					Dogus: map[common.SimpleDoguName]domain.CombinedDoguConfig{
+						nginxStaticQualifiedDoguName.SimpleName: {
+							DoguName: nginxStaticQualifiedDoguName.SimpleName,
+							Config: domain.DoguConfig{
+								Present: map[common.DoguConfigKey]common.DoguConfigValue{
+									nginxStaticConfigKeyNginxKey1: "nginxVal1",
+								},
+								Absent: []common.DoguConfigKey{
+									nginxStaticConfigKeyNginxKey2,
+								},
+							},
+							SensitiveConfig: domain.SensitiveDoguConfig{},
+						},
+					},
+				},
+			},
+			Status: domain.StatusPhaseValidated,
+		}
+
+		blueprintRepoMock := newMockBlueprintSpecRepository(t)
+		blueprintRepoMock.EXPECT().GetById(testCtx, "testBlueprint1").Return(blueprint, nil)
+		blueprintRepoMock.EXPECT().Update(testCtx, blueprint).Return(nil)
+
+		doguInstallRepoMock := newMockDoguInstallationRepository(t)
+		installedDogus := map[common.SimpleDoguName]*ecosystem.DoguInstallation{
+			"nginx-static": {Name: nginxStaticQualifiedDoguName, Version: mustParseVersion(t, "1.8.6")},
+		}
+		doguInstallRepoMock.EXPECT().GetAll(testCtx).Return(installedDogus, nil)
+		componentInstallRepoMock := newMockComponentInstallationRepository(t)
+		componentInstallRepoMock.EXPECT().GetAll(testCtx).Return(nil, nil)
+		globalConfigRepoMock := NewMockGlobalConfigEntryRepository(t)
+		globalConfigRepoMock.EXPECT().GetAllByKey(testCtx, blueprint.EffectiveBlueprint.Config.Global.GetGlobalConfigKeys()).Return(map[common.GlobalConfigKey]*ecosystem.GlobalConfigEntry{}, nil)
+		doguConfigRepoMock := NewMockDoguConfigEntryRepository(t)
+		doguConfigRepoMock.EXPECT().
+			GetAllByKey(testCtx, []common.DoguConfigKey{
+				nginxStaticConfigKeyNginxKey1,
+				nginxStaticConfigKeyNginxKey2,
+			}).
+			Return(map[common.DoguConfigKey]*ecosystem.DoguConfigEntry{
+				nginxStaticConfigKeyNginxKey1: {
+					Key:   nginxStaticConfigKeyNginxKey1,
+					Value: "val1",
+				},
+				nginxStaticConfigKeyNginxKey2: {
+					Key:   nginxStaticConfigKeyNginxKey2,
+					Value: "val2",
+				},
+			}, nil)
+		sensitiveDoguConfigRepoMock := NewMockSensitiveDoguConfigEntryRepository(t)
+		sensitiveDoguConfigRepoMock.EXPECT().
+			GetAllByKey(testCtx, []common.SensitiveDoguConfigKey(nil)).
+			Return(map[common.SensitiveDoguConfigKey]*ecosystem.SensitiveDoguConfigEntry{}, nil)
+
+		sut := NewStateDiffUseCase(blueprintRepoMock, doguInstallRepoMock, componentInstallRepoMock, globalConfigRepoMock, doguConfigRepoMock, sensitiveDoguConfigRepoMock)
+
+		// when
+		err := sut.DetermineStateDiff(testCtx, "testBlueprint1")
+
+		// then
+		require.NoError(t, err)
+
+		expectedConfigDiff := map[common.SimpleDoguName]domain.CombinedDoguConfigDiffs{
+			"nginx-static": {
+				DoguConfigDiff: []domain.DoguConfigEntryDiff{
+					{
+						Key:          nginxStaticConfigKeyNginxKey1,
+						Actual:       domain.DoguConfigValueState{Value: "val1", Exists: true},
+						Expected:     domain.DoguConfigValueState{Value: "nginxVal1", Exists: true},
+						NeededAction: domain.ConfigActionSet,
+					},
+					{
+						Key:          nginxStaticConfigKeyNginxKey2,
+						Actual:       domain.DoguConfigValueState{Value: "val2", Exists: true},
+						Expected:     domain.DoguConfigValueState{Value: "", Exists: false},
+						NeededAction: domain.ConfigActionRemove,
+					},
+				},
+				SensitiveDoguConfigDiff: nil,
+			},
+		}
+		assert.Equal(t, expectedConfigDiff, blueprint.StateDiff.DoguConfigDiffs)
+	})
+	t.Run("should succeed for sensitive dogu config diff", func(t *testing.T) {
+		// given
+		blueprint := &domain.BlueprintSpec{
+			Id: "testBlueprint1",
+			EffectiveBlueprint: domain.EffectiveBlueprint{
+				Config: domain.Config{
+					Dogus: map[common.SimpleDoguName]domain.CombinedDoguConfig{
+						nginxStaticQualifiedDoguName.SimpleName: {
+							DoguName: nginxStaticQualifiedDoguName.SimpleName,
+							SensitiveConfig: domain.SensitiveDoguConfig{
+								Present: map[common.SensitiveDoguConfigKey]common.SensitiveDoguConfigValue{
+									nginxStaticSensitiveConfigKeyNginxKey1: "nginxVal1",
+								},
+								Absent: []common.SensitiveDoguConfigKey{
+									nginxStaticSensitiveConfigKeyNginxKey2,
+								},
+							},
+						},
+					},
+				},
+			},
+			Status: domain.StatusPhaseValidated,
+		}
+
+		blueprintRepoMock := newMockBlueprintSpecRepository(t)
+		blueprintRepoMock.EXPECT().GetById(testCtx, "testBlueprint1").Return(blueprint, nil)
+		blueprintRepoMock.EXPECT().Update(testCtx, blueprint).Return(nil)
+
+		doguInstallRepoMock := newMockDoguInstallationRepository(t)
+		installedDogus := map[common.SimpleDoguName]*ecosystem.DoguInstallation{
+			"nginx-static": {Name: nginxStaticQualifiedDoguName, Version: mustParseVersion(t, "1.8.6")},
+		}
+		doguInstallRepoMock.EXPECT().GetAll(testCtx).Return(installedDogus, nil)
+		componentInstallRepoMock := newMockComponentInstallationRepository(t)
+		componentInstallRepoMock.EXPECT().GetAll(testCtx).Return(nil, nil)
+		globalConfigRepoMock := NewMockGlobalConfigEntryRepository(t)
+		globalConfigRepoMock.EXPECT().GetAllByKey(testCtx, blueprint.EffectiveBlueprint.Config.Global.GetGlobalConfigKeys()).Return(map[common.GlobalConfigKey]*ecosystem.GlobalConfigEntry{}, nil)
+		doguConfigRepoMock := NewMockDoguConfigEntryRepository(t)
+		doguConfigRepoMock.EXPECT().GetAllByKey(testCtx, []common.DoguConfigKey(nil)).Return(nil, nil)
+		sensitiveDoguConfigRepoMock := NewMockSensitiveDoguConfigEntryRepository(t)
+		sensitiveDoguConfigRepoMock.EXPECT().
+			GetAllByKey(testCtx, []common.SensitiveDoguConfigKey{
+				nginxStaticSensitiveConfigKeyNginxKey1,
+				nginxStaticSensitiveConfigKeyNginxKey2,
+			}).
+			Return(map[common.SensitiveDoguConfigKey]*ecosystem.SensitiveDoguConfigEntry{
+				nginxStaticSensitiveConfigKeyNginxKey1: {
+					Key:   nginxStaticSensitiveConfigKeyNginxKey1,
+					Value: "val1",
+				},
+				nginxStaticSensitiveConfigKeyNginxKey2: {
+					Key:   nginxStaticSensitiveConfigKeyNginxKey2,
+					Value: "val2",
+				},
+			}, nil)
+
+		sut := NewStateDiffUseCase(blueprintRepoMock, doguInstallRepoMock, componentInstallRepoMock, globalConfigRepoMock, doguConfigRepoMock, sensitiveDoguConfigRepoMock)
+
+		// when
+		err := sut.DetermineStateDiff(testCtx, "testBlueprint1")
+
+		// then
+		require.NoError(t, err)
+
+		expectedConfigDiff := map[common.SimpleDoguName]domain.CombinedDoguConfigDiffs{
+			"nginx-static": {
+				SensitiveDoguConfigDiff: []domain.SensitiveDoguConfigEntryDiff{
+					{
+						Key:                  nginxStaticSensitiveConfigKeyNginxKey1,
+						Actual:               domain.DoguConfigValueState{Value: "val1", Exists: true},
+						Expected:             domain.DoguConfigValueState{Value: "nginxVal1", Exists: true},
+						NeededAction:         domain.ConfigActionSet,
+						DoguAlreadyInstalled: true,
+					},
+					{
+						Key:                  nginxStaticSensitiveConfigKeyNginxKey2,
+						Actual:               domain.DoguConfigValueState{Value: "val2", Exists: true},
+						Expected:             domain.DoguConfigValueState{Value: "", Exists: false},
+						NeededAction:         domain.ConfigActionRemove,
+						DoguAlreadyInstalled: true,
+					},
+				},
+			},
+		}
+		assert.Equal(t, expectedConfigDiff, blueprint.StateDiff.DoguConfigDiffs)
 	})
 }
 
