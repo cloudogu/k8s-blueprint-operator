0802591b61539b45db4ff5e74960ab06e137c01c
Niklas <niklas.hinze@cloudogu.com>
2024-02-26 09:41:25 +0100

    #39 Add rbac for getting secrets.
    This is necessary to read the dogu private keys.

--- pkg/application/registryConfigUseCase.go
+++ pkg/application/registryConfigUseCase.go
@@ -8,15 +8,8 @@ import (
 	"github.com/cloudogu/k8s-blueprint-operator/pkg/domain"
 	"github.com/cloudogu/k8s-blueprint-operator/pkg/domain/common"
 	"github.com/cloudogu/k8s-blueprint-operator/pkg/domain/ecosystem"
-	"k8s.io/apimachinery/pkg/util/yaml"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 )
-
-//go:embed testdata/doguConfigDiffMockData.yaml
-var doguConfigDiffMockDataBytes []byte
-
-//go:embed testdata/globalConfigDiffMockData.yaml
-var globalConfigDiffMockDataBytes []byte
 
 type EcosystemRegistryUseCase struct {
 	blueprintRepository           blueprintSpecRepository
@@ -43,19 +36,6 @@ func (useCase *EcosystemRegistryUseCase) ApplyConfig(ctx context.Context, bluepr
 	if err != nil {
 		return fmt.Errorf("cannot load blueprint to apply config: %w", err)
 	}
-
-	// TODO Remove this before merge.
-	// stage := os.Getenv("STAGE")
-	// if stage == "development" {
-	// 	logger.Info("set config diffs from mock data...")
-	// 	data := parseDoguConfigDiffMockData()
-	// 	logger.Info("dogu config diffs:")
-	// 	logger.Info(fmt.Sprintf("%+v", data))
-	// 	blueprintSpec.StateDiff.DoguConfigDiffs = data
-	// 	logger.Info("dogu config diffs in statediff:")
-	// 	logger.Info(fmt.Sprintf("%+v", blueprintSpec.StateDiff.DoguConfigDiff))
-	// 	blueprintSpec.StateDiff.GlobalConfigDiffs = parseGlobalConfigDiffMockData()
-	// }
 
 	doguConfigDiffs := blueprintSpec.StateDiff.DoguConfigDiffs
 	isEmptyDoguDiff := len(doguConfigDiffs) == 0
@@ -92,26 +72,6 @@ func (useCase *EcosystemRegistryUseCase) ApplyConfig(ctx context.Context, bluepr
 	}
 
 	return useCase.markConfigApplied(ctx, blueprintSpec)
-}
-
-func parseGlobalConfigDiffMockData() domain.GlobalConfigDiffs {
-	object := &domain.GlobalConfigDiffs{}
-	err := yaml.Unmarshal(globalConfigDiffMockDataBytes, object)
-	if err != nil {
-		panic(fmt.Errorf("error during mock data deserialization"))
-	}
-
-	return *object
-}
-
-func parseDoguConfigDiffMockData() map[common.SimpleDoguName]domain.CombinedDoguConfigDiffs {
-	object := &map[common.SimpleDoguName]domain.CombinedDoguConfigDiffs{}
-	err := yaml.Unmarshal(doguConfigDiffMockDataBytes, object)
-	if err != nil {
-		panic(fmt.Errorf("error during mock data deserialization"))
-	}
-
-	return *object
 }
 
 func (useCase *EcosystemRegistryUseCase) applyGlobalConfigDiffs(ctx context.Context, diffs domain.GlobalConfigDiffs) error {
--- pkg/application/testdata/createEntryToDelete.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-etcdClient="$(kubectl get pods -l=app.kubernetes.io/name=etcd-client -o name 2>&1 | head -n 1)"
-
-kubectl exec --namespace ecosystem "$etcdClient" -- etcdctl set "/config/_global/global_delete" "doesnotmatter" && \
-kubectl exec --namespace ecosystem "$etcdClient" -- etcdctl set "/config/postgresql/example_key_to_remove" "doesnotmatter" && \
-kubectl exec --namespace ecosystem "$etcdClient" -- etcdctl set "/config/postgresql/example_key_to_remove_encrypted" "doesnotmatter"
