7b15a811707ad1b1a719374602e917ab548c674b
Alexander Dammeier <alexander.dammeier@cloudogu.com>
2024-10-16 09:30:03 +0200

    #81 remove code for old maintenance mode
    later it will be replaced with the new one
    from the k8s-registry-lib.
    
    Removing it now enables us to test the rest
    of the new code.

--- pkg/adapter/maintenance/mode.go
+++ pkg/adapter/maintenance/mode.go
@@ -9,56 +9,20 @@ import (
 // Mode contains methods to Activate and Deactivate the switcher.
 // When it is active, a page is displayed to the user, telling them that there is maintenance going on.
 type Mode struct {
-	lock
-	switcher
 }
 
 func New(globalConfig registry.ConfigurationContext) *Mode {
-	return &Mode{
-		lock:     &defaultLock{globalConfig: globalConfig},
-		switcher: &defaultSwitcher{globalConfig: globalConfig},
-	}
+	return &Mode{}
 }
 
 // Activate enables the maintenance mode, setting the given MaintenancePageModel
 func (m *Mode) Activate(content domainservice.MaintenancePageModel) error {
-	isActive, isOurs, err := m.lock.isActiveAndOurs()
-	if err != nil {
-		return domainservice.NewInternalError(err, "failed to check if maintenance mode is already active and ours")
-	}
-
-	if isActive && !isOurs {
-		return domainservice.NewConflictError(nil, "cannot activate maintenance mode as it was already activated by another party")
-	}
-
-	err = m.switcher.activate(content)
-	if err != nil {
-		return domainservice.NewInternalError(err, "failed to activate maintenance mode")
-	}
-
+	// TODO: replace this completely with maintenance mode from k8s-registry-lib
 	return nil
 }
 
 // Deactivate disables the maintenance mode.
 func (m *Mode) Deactivate() error {
-	isActive, isOurs, err := m.lock.isActiveAndOurs()
-	if err != nil {
-		return domainservice.NewInternalError(err, "failed to check if maintenance mode is already active and ours")
-	}
-
-	if !isActive {
-		// do nothing
-		return nil
-	}
-
-	if !isOurs {
-		return domainservice.NewConflictError(nil, "cannot deactivate maintenance mode as it was activated by another party")
-	}
-
-	err = m.switcher.deactivate()
-	if err != nil {
-		return domainservice.NewInternalError(err, "failed to deactivate maintenance mode")
-	}
-
+	// TODO: replace this completely with maintenance mode from k8s-registry-lib
 	return nil
 }
